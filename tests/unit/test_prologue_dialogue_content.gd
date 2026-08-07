e4f811ed3c965a8917eb6fcbab330ebbf05f4209
sulcjo <sulcjo95@gmail.com>
2026-08-08 01:56:24 +0200

    content: add Chapter 0 Prologue dialogue tree

---
+++ tests/unit/test_prologue_dialogue_content.gd
@@ -0,0 +1,44 @@
+extends GutTest
+
+func _load_nodes() -> Array:
+	var file := FileAccess.open("res://content/chapters/chapter_00_prologue/prologue.json", FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	file.close()
+	return data
+
+func test_every_next_id_points_at_a_node_that_exists():
+	var nodes := _load_nodes()
+	var known_ids: Dictionary = {}
+	for node in nodes:
+		known_ids[node["id"]] = true
+	for node in nodes:
+		for choice in node.get("choices", []):
+			assert_true(known_ids.has(choice["next_id"]), "%s -> next_id '%s' does not exist" % [node["id"], choice["next_id"]])
+
+func test_exactly_one_node_has_no_choices_and_it_is_the_last_node():
+	var nodes := _load_nodes()
+	var end_node_ids: Array = []
+	for node in nodes:
+		if node.get("choices", []).is_empty():
+			end_node_ids.append(node["id"])
+	assert_eq(end_node_ids, ["n12_departure"])
+
+func test_every_glossed_term_id_exists_in_the_prologue_glossary():
+	var nodes := _load_nodes()
+	var glossary_file := FileAccess.open("res://content/glossary/prologue_terms.json", FileAccess.READ)
+	var glossary_data = JSON.parse_string(glossary_file.get_as_text())
+	glossary_file.close()
+
+	for node in nodes:
+		for term_id in GlossedTextParser.extract_term_ids(node["text"]):
+			assert_true(glossary_data.has(term_id), "node %s glosses unknown term '%s'" % [node["id"], term_id])
+
+func test_the_full_tree_is_walkable_from_start_to_end_via_first_choices():
+	var engine := DialogueEngine.new()
+	engine.load_tree(_load_nodes(), "n01_naming")
+	var visited := 0
+	while not engine.is_chapter_end() and visited < 100:
+		engine.choose(0)
+		visited += 1
+	assert_true(engine.is_chapter_end())
+	assert_eq(engine.current_node()["id"], "n12_departure")
