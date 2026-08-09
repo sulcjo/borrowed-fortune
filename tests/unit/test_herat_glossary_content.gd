e05366587bd1fa113d17352d64dc8cceccd82267
sulcjo <sulcjo95@gmail.com>
2026-08-09 16:49:30 +0200

    feat: add Herat glossary terms

---
+++ tests/unit/test_herat_glossary_content.gd
@@ -0,0 +1,15 @@
+extends GutTest
+
+func test_herat_glossary_has_the_two_expected_terms_with_headword_and_definition():
+	var file := FileAccess.open("res://content/glossary/herat_terms.json", FileAccess.READ)
+	var data = JSON.parse_string(file.get_as_text())
+	file.close()
+
+	var expected_ids := ["hashar", "dai"]
+	assert_eq(data.keys().size(), expected_ids.size())
+	for term_id in expected_ids:
+		assert_true(data.has(term_id), "missing glossary term '%s'" % term_id)
+		assert_true(data[term_id].has("headword"))
+		assert_true(data[term_id].has("definition"))
+		assert_false(data[term_id]["headword"].is_empty())
+		assert_false(data[term_id]["definition"].is_empty())
